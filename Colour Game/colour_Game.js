0509d1f0e4736f53b2a03269a4c5bc579788f908
dounisca <dounisca@yahoo.com>
2020-04-14 15:43:38 +0100

    Updated the reset button to respond to a correct answer

--- Colour Game/colour_Game.js
+++ Colour Game/colour_Game.js
@@ -18,6 +18,7 @@ resetButton.addEventListener('click', function(){
     for(var i = 0; i < squares.length; i++){
         squares[i].style.backgroundColor = colors[i];
     }
+    h1.style.backgroundColor = '#232323';
 });
 
 colorDisplay.textContent = pickedColour;
@@ -36,6 +37,7 @@ for(var i = 0; i < squares.length; i++){
             messageDisplay.textContent = "Correct!";
             changeColors(clickedColor);
             h1.style.backgroundColor = clickedColor;
+            resetButton.textContent = "Play Again";
         } else {
             this.style.backgroundColor = "#232323";
             messageDisplay.textContent = "Try Again"
