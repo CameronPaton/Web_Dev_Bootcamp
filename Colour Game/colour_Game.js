623e6ea60ff3ad293682d013431ef73936d5358e
dounisca <dounisca@yahoo.com>
2020-04-14 11:29:03 +0100

    Adding a loop and Event Listener to the JS file

--- Colour Game/colour_Game.js
+++ Colour Game/colour_Game.js
@@ -1,4 +1,4 @@
-var colours = [
+var colors = [
     'rgb(255, 0, 0)',
     'rgb(255, 255, 0)',
     'rgb(0, 255, 0)',
@@ -8,7 +8,26 @@ var colours = [
 ]
 
 var squares = document.querySelectorAll(".square");
+var pickedColour = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
+
+colorDisplay.textContent = pickedColour;
 
 for(var i = 0; i < squares.length; i++){
-    squares[i].style.backgroundColor = colours[i];
+    //add initial colours to squares from the colours var
+    squares[i].style.backgroundColor = colors[i];
+
+    //add click listeners to squares so that when the square is clicked - further actions will occur
+
+    squares[i].addEventListener("click", function(){
+        //grab colour of clicked square
+        var clickedColor = this.style.backgroundColor;
+        //compare colour to picked colour var
+        if(clickedColor === pickedColour){
+            alert("correct")
+        } else {
+            alert("wrong");
+        };
+    });
+    
 }
