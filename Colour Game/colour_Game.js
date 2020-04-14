28c61ed34fa7362de572c60c57cc045c2ee7881d
dounisca <dounisca@yahoo.com>
2020-04-14 13:58:41 +0100

    added a function to the JS file to randomise the colours when the page is refreshed

--- Colour Game/colour_Game.js
+++ Colour Game/colour_Game.js
@@ -1,11 +1,4 @@
-var colors = [
-    'rgb(255, 0, 0)',
-    'rgb(255, 255, 0)',
-    'rgb(0, 255, 0)',
-    'rgb(0, 255, 255)',
-    'rgb(0, 0, 255)',
-    'rgb(255, 0, 255)',
-]
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
 var pickedColour = pickColor();
@@ -46,4 +39,27 @@ function changeColors(color){
 function pickColor(){
     var random = Math.floor(Math.random() * colors.length);
     return colors[random];
+}
+
+function generateRandomColors(num){
+    //make an array
+    var arr = [];
+    //add num random colors to array
+    for(var i = 0; i < num; i++){
+        //get a random number and put it into the array
+        arr.push(randomColor());
+    }
+    //return random colors
+    return arr;
+}
+
+function randomColor(){
+    //pick a red from 0 - 255
+    var r = Math.floor(Math.random() * 256)
+    //pick a green from 0 - 255
+    var g = Math.floor(Math.random() * 256)
+    //pick a blue from 0 - 255
+    var b = Math.floor(Math.random() * 256)
+    //to generate a random number between 0 and 255
+    return "rgb(" + r + ", " + g + ", " + b + ")";
 }
