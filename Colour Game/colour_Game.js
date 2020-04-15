8d65469e2a2d975246b073791e2b7a6080bb9bc5
dounisca <dounisca@yahoo.com>
2020-04-15 12:26:04 +0100

    Adding the Easy and Hard button functionality

--- Colour Game/colour_Game.js
+++ Colour Game/colour_Game.js
@@ -1,4 +1,5 @@
-var colors = generateRandomColors(6);
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
 
 var squares = document.querySelectorAll(".square");
 var pickedColour = pickColor();
@@ -6,10 +7,42 @@ var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector('#message');
 var h1 = document.querySelector('h1');
 var resetButton = document.getElementById('resetButton');
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+
+easyBtn.addEventListener("click", function(){
+    hardBtn.classList.remove("selected");
+    easyBtn.classList.add("selected");
+    numSquares = 3;
+    colors = generateRandomColors(numSquares);
+    pickedColour = pickColor();
+    colorDisplay.textContent = pickedColour;
+    for(var i = 0; i < squares.length; i++){
+        if(colors[i]){
+            squares[i].style.background = colors[i];
+        } else {
+            squares[i].style.display = "none";
+        }
+    }
+});
+
+hardBtn.addEventListener("click", function(){
+    hardBtn.classList.add("selected");
+    easyBtn.classList.remove("selected");
+    numSquares = 6;
+    colors = generateRandomColors(numSquares);
+    pickedColour = pickColor();
+    colorDisplay.textContent = pickedColour;
+    for(var i = 0; i < squares.length; i++){
+        squares[i].style.background = colors[i];
+        squares[i].style.display = "block";
+    };
+});
 
 resetButton.addEventListener('click', function(){
     // generate new colours
-    colors = generateRandomColors(6);
+    colors = generateRandomColors(numSquares);
     //pick a new random colour from the array
     pickedColour = pickColor();
     //change colours of the squares on page
