19ed2fefb0a178b632a9592d59a3720d36aaecb4
dounisca <dounisca@yahoo.com>
2020-04-16 15:43:13 +0100

    Created the JS add to do functionality

--- jQuery_To_Do/assets/js/to_do.js
+++ jQuery_To_Do/assets/js/to_do.js
@@ -1,15 +1,27 @@
 //toggling task completion
 
-$('li').click(function(){
+$("ul").on("click", "li",  function(){
     $(this).toggleClass('completed');
 });
 
 //deleting to dos by clicking x
 
-$("span").click(function(e){
+$("ul").on("click", "span", function(e){
     $(this).parent().fadeOut("500", function(){
         $(this).remove();
     })
     e.stopPropagation();
+});
+
+// creating to dos
+
+$("input[type='text']").keypress(function(e){
+    if(e.which === 13){
+        //grabbing new to do text from input
+       var todotext = $(this).val();
+       $(this).val("");
+       //create a new li and add to ul
+       $("ul").append("<li><span> X </span> " + todotext + "</li>");    
+    }
 });
 
