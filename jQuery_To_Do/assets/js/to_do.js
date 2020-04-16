b9c39f36b6dc150c86cb619de47af2804bf83062
dounisca <dounisca@yahoo.com>
2020-04-16 14:30:44 +0100

    adding the delete to dos functionality

--- jQuery_To_Do/assets/js/to_do.js
+++ jQuery_To_Do/assets/js/to_do.js
@@ -4,5 +4,12 @@ $('li').click(function(){
     $(this).toggleClass('completed');
 });
 
-//deleting to dos
+//deleting to dos by clicking x
+
+$("span").click(function(e){
+    $(this).parent().fadeOut("500", function(){
+        $(this).remove();
+    })
+    e.stopPropagation();
+});
 
