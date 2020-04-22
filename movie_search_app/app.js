84421b66d363cdb483eff944a3306d9940087348
dounisca <dounisca@yahoo.com>
2020-04-22 22:20:00 +0100

    added the ability for the user to control the search criteria

--- movie_search_app/app.js
+++ movie_search_app/app.js
@@ -3,14 +3,22 @@ const app = express();
 const request = require("request");
 app.set("view engine", "ejs");
 
+app.get("/search", function(req, res){
+    res.render("search");
+});
+
 app.get("/results", function(req, res){
-    request("http://omdbapi.com/?s=london&apikey=thewdb", function(error, response, body){
+    const query = req.query.search;
+    const apiKey = "&apikey=thewdb";
+    const url = "http://omdbapi.com/?s=" + query + apiKey;
+
+    request(url, function(error, response, body){
         if(!error && response.statusCode == 200){
             const data = JSON.parse(body);
             res.render("results", {data: data});
         };
     })
-})
+});
 
 app.listen(3000, function(){
     console.log("the server has started!!");
