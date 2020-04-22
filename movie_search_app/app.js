0666744b99a745d4ff0dfe630838dc7995faa428
dounisca <dounisca@yahoo.com>
2020-04-22 16:14:50 +0100

    results ejs page now displays the data in an li

--- movie_search_app/app.js
+++ movie_search_app/app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
 const request = require("request");
+app.set("view engine", "ejs");
 
 app.get("/results", function(req, res){
-    request("http://omdbapi.com/?s=california&apikey=thewdb", function(error, response, body){
+    request("http://omdbapi.com/?s=london&apikey=thewdb", function(error, response, body){
         if(!error && response.statusCode == 200){
-            const results = JSON.parse(body);
-            res.send(results["Search"][0]["Title"]);
+            const data = JSON.parse(body);
+            res.render("results", {data: data});
         };
     })
 })
