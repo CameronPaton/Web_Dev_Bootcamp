fa211249e79bc9f593efee12bb97fdfe61d16cff
dounisca <dounisca@yahoo.com>
2020-04-22 15:42:31 +0100

    Adding the first route which is a get request for a search of a movie database for all movies with California in the title, and sending back in the res.send the title of the first item in the list of results. The JSON content has also been parsed from a string to an object

--- movie_search_app/app.js
+++ movie_search_app/app.js
@@ -3,9 +3,16 @@ const app = express();
 const request = require("request");
 
 app.get("/results", function(req, res){
-    res.send("Homepage");
+    request("http://omdbapi.com/?s=california&apikey=thewdb", function(error, response, body){
+        if(!error && response.statusCode == 200){
+            const results = JSON.parse(body);
+            res.send(results["Search"][0]["Title"]);
+        };
+    })
 })
 
 app.listen(3000, function(){
     console.log("the server has started!!");
-})
+});
+
+//&apikey=thewdb
