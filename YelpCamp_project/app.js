3cc8c4426d7e3362d3b00f5c75c6178a03f6a40b
dounisca <dounisca@yahoo.com>
2020-04-23 13:52:53 +0100

    Added in some campgrounds to an array with names and image objects and looped through it to display

--- YelpCamp_project/app.js
+++ YelpCamp_project/app.js
@@ -1,10 +1,19 @@
-const express = require("express");
-const app = express();
+var express = require("express");
+var app = express();
+app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
-    res.send("Welcome to the homepage");
+    res.render("landing");
 });
 
+app.get("/campgrounds", function(req, res){
+    var campgrounds = [
+        {name: "Salmon Creek", image: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"},
+        {name: "Granite Hill", image: "https://images.unsplash.com/photo-1532339142463-fd0a8979791a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"},
+        {name: "Mountain Goats Rest", image: "https://images.unsplash.com/photo-1504851149312-7a075b496cc7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=698&q=80"}
+    ];
+    res.render("campgrounds", {campgrounds: campgrounds});
+});
 
 app.listen(3000, function(){
     console.log("server has started");
