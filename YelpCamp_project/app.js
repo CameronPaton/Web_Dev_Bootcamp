4c18766d76d35737e31c104db517a9a789ebf9f7
dounisca <dounisca@yahoo.com>
2020-04-23 16:01:18 +0100

    Adding some nav buttons

--- YelpCamp_project/app.js
+++ YelpCamp_project/app.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require("body-parser");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req, res){
     res.render("landing");
 });
